892bf27ad5b5085295bcda919cbe654ff0212258
Martin Delille <martin@phonations.com>
2015-10-02 16:59:27 +0200

    delete todo

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -19,4 +19,7 @@ angular.module('yeomanTutorialApp')
       $scope.todos.push($scope.todo);
       $scope.todo = '';
     };
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
+    };
   });
